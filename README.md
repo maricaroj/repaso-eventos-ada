6e82e538890884781bc42463723a8722f068a89c
maricaroj <juarezmariana18@gmail.com>
2021-02-03 00:28:12 -0300

    cambio

---
+++ README.md
@@ -0,0 +1,13 @@
+## Pseudocódigo sección URL de la imagen
+
+### ¿Cómo logramos que al poner la url en el input se pinte la imagen en el contenedor correspondiente?
+
+- Inicio 
+- Tener identificado el de donde sacaremos el link (llamar al id)
+- Identificar cuando el usuario ingrese el link
+- Guardar el link que ingresó el usuario
+- Identificar el lugar donde se pintara la imagen 
+- Agregarle el link como atributo 
+- Fin 
+
+![DFD Seccion URL img](./img/seccion-url-img.png)
--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-// Elemtos a los que le aplicaremos el innerHTML o donde se ingresa el DOM
+// Elementos a los que le aplicaremos el innerHTML o donde se ingresa el DOM
 const primerTexto = document.getElementById('primer-texto');
+const segundoTexto = document.getElementById('segundo-texto');
+const color = document.getElementById('color');
+const fondo = document.getElementById('fondo');
+const colorText = document.getElementById('color-text');
+const backColor = document.getElementById('back-color');
+const centerText = document.getElementById('center-text');
 
 // Elementos a los que se les aplicara los eventos
 const topInput = document.getElementById('top-input');
@@ -7,6 +13,10 @@ const topCheck = document.getElementById('top-check');
 const option = document.getElementById('option');
 const inputSize = document.getElementById('input-size');
 const izquierda = document.getElementById('izquierda');
+const derecha = document.getElementById('derecha');
+const centrar = document.getElementById('centrar');
+const transparente = document.getElementById('transparente')
+const urlImg = document.getElementById('url-img');
 
 topInput.addEventListener('keyup', () => {
   primerTexto.innerHTML = topInput.value;
@@ -15,13 +25,13 @@ topInput.addEventListener('keyup', () => {
 
 topCheck.addEventListener('click',() => {
   if(topCheck.checked){
-    // primerTexto.style.display = 'none';
-    // primerTexto.classList.add('ocultar');
+    primerTexto.style.display = 'none';
+    primerTexto.classList.add('ocultar');
   } else {
-    // primerTexto.style.display = 'block';
+    primerTexto.style.display = 'block';
     // primerTexto.style.color = 'red';
-    // primerTexto.style.backGround = 'blue';
-    // primerTexto.classList.remove('ocultar');
+    primerTexto.style.backGround = 'blue';
+    primerTexto.classList.remove('ocultar');
   }
 });
 
@@ -41,6 +51,50 @@ izquierda.addEventListener('click', (e) => {
   e.preventDefault();
   console.log(izquierda.id);
   primerTexto.style.textAlign = 'left';
+  segundoTexto.style.textAlign = 'left';
+});
+
+derecha.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(derecha.id);
+  primerTexto.style.textAlign = 'right';
+  segundoTexto.style.textAlign = 'right';
+});
+
+centrar.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(centrar.id);
+  primerTexto.style.textAlign = 'center';
+  segundoTexto.style.textAlign = 'center';
+});
+
+color.addEventListener('input', (e)=>{
+  const textColor = e.target.value;
+  primerTexto.style.color = textColor;
+  segundoTexto.style.color = textColor;
+  colorText.innerHTML = textColor.toUpperCase();
+})
+
+fondo.addEventListener('input', (e)=>{
+  const textColor = e.target.value;
+  primerTexto.style.backgroundColor = textColor;
+  segundoTexto.style.backgroundColor = textColor;
+  backColor.innerHTML = textColor.toUpperCase();
+})
+transparente.addEventListener('click', (e) => {
+  if(e.target.checked){
+    // primerTexto.style.backgroundColor = 'rgba(0,0,0, 0.0)'
+
+  }
+})
+
+urlImg.addEventListener('keyup', ()=>{
+  const src = urlImg.value;
+  console.log(src);
+  centerText.style.backgroundImage = `url('${src}')`;
+  centerText.style.backgroundPosition = 'center';
+  centerText.style.backgroundRepeat = 'no-repeat';
+  centerText.style.backgroundSize = 'contain';
 });
 
 
